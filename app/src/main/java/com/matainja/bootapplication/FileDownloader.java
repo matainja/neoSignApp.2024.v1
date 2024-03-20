c7e37bfe2c4c5a3b42df407540e686d9e242eb62
Suman Nandi <matainja066@gmail.com>
2024-03-20 20:00:09 +0530

    20.03.2024

--- app/src/main/java/com/matainja/bootapplication/FileDownloader.java
+++ app/src/main/java/com/matainja/bootapplication/FileDownloader.java
@@ -1,4 +1,93 @@
 package com.matainja.bootapplication;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
+
+public class FileDownloader extends AsyncTask<String, Void, String> {
+
+    private Context context;
+    private OnDownloadCompleteListener listener;
+
+    public interface OnDownloadCompleteListener {
+        void onDownloadComplete(String filePath);
+        void onError(String errorMessage);
+        void onDownloadStatus(boolean isDownloading);
+    }
+
+    public FileDownloader(Context context, OnDownloadCompleteListener listener) {
+        this.context = context;
+        this.listener = listener;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        listener.onDownloadStatus(true);
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String fileUrl = params[0];
+        try {
+            URL url = new URL(fileUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(10000); // Connection timeout in milliseconds
+
+            Random random = new Random();
+            int randomNumber = random.nextInt(100);
+
+            File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+            if (directory != null) {
+                if (!directory.exists()) {
+                    directory.mkdirs(); // Create directories if they don't exist
+                }
+                File file = new File(directory, "downloaded_video" + randomNumber + ".mp4");
+                FileOutputStream outputStream = new FileOutputStream(file);
+                InputStream inputStream = connection.getInputStream();
+
+                byte[] buffer = new byte[2048];
+                int bytesRead;
+
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+
+                outputStream.close();
+                inputStream.close();
+
+                return file.getAbsolutePath();
+            } else {
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null; // Return null in case of error
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        if (result != null) {
+            listener.onDownloadComplete(result);
+        } else {
+            listener.onError("Error downloading file");
+        }
+        listener.onDownloadStatus(false); // Notify that download is complete
+    }
+}
+
+
+/*
+package com.matainja.bootapplication;
 
 import android.content.Context;
 import android.os.AsyncTask;
@@ -89,3 +178,4 @@ public class FileDownloader extends AsyncTask<String, Void, String> {
     }
 
 }
+*/
